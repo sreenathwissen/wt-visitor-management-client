ae413928c226f942a1d82cd01926d8a660cecd23
saad95544 <mohammad.saad@wissen.com>
2023-04-05 23:51:40 +0530

    changes for date range picker for inTime and outTime

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -17,8 +17,10 @@ export class FilterComponent implements OnInit {
     location: [''],
     visitorType: [''],
     idProofType: [''],
-    inTime: [''],
-    outTime: [''],
+    inTimeFrom: [''],
+    inTimeTo: [''],
+    outTimeFrom: [''],
+    outTimeTo: [''],
   });
   ngOnInit(): void {}
   onSubmit() {
@@ -26,15 +28,56 @@ export class FilterComponent implements OnInit {
     (
       Object.keys(this.formGroup.controls) as (keyof typeof FormGroup)[]
     ).forEach((key: any, index) => {
-      if (this.formGroup.controls[key].touched) {
+      if (
+        JSON.stringify(filterParams).indexOf('inTime') === -1 &&
+        (key === 'inTimeFrom' || key === 'inTimeTo') &&
+        (this.formGroup.controls['inTimeFrom'].touched ||
+          this.formGroup.controls['inTimeTo'].touched)
+      ) {
+        this.formGroup.controls['inTimeFrom'].setValue(
+          this.formGroup.controls['inTimeFrom'].value || '2023-01-01'
+        );
+        this.formGroup.controls['inTimeTo'].setValue(
+          this.formGroup.controls['inTimeTo'].value ||
+            new Date().toISOString().split('T')[0]
+        );
+        filterParams.push({
+          fieldName: 'inTime',
+          operator: 'BETWEEN',
+          values: [
+            new Date(this.formGroup.controls['inTimeFrom'].value),
+            new Date(this.formGroup.controls['inTimeTo'].value),
+          ],
+        });
+      } else if (
+        JSON.stringify(filterParams).indexOf('outTime') === -1 &&
+        (key === 'outTimeFrom' || key === 'outTimeTo') &&
+        (this.formGroup.controls['outTimeFrom'].touched ||
+          this.formGroup.controls['outTimeTo'].touched)
+      ) {
+        this.formGroup.controls['outTimeFrom'].setValue(
+          this.formGroup.controls['outTimeFrom'].value || '2023-01-01'
+        );
+        this.formGroup.controls['outTimeTo'].setValue(
+          this.formGroup.controls['outTimeTo'].value ||
+            new Date().toISOString().split('T')[0]
+        );
+        filterParams.push({
+          fieldName: 'outTime',
+          operator: 'BETWEEN',
+          values: [
+            new Date(this.formGroup.controls['outTimeFrom'].value),
+            new Date(this.formGroup.controls['outTimeTo'].value),
+          ],
+        });
+      } else if (
+        key.indexOf('Time') === -1 &&
+        this.formGroup.controls[key].touched
+      ) {
         filterParams.push({
           fieldName: key,
           operator: 'LIKE',
-          values: [
-            key === 'inTime' || key === 'outTime'
-              ? new Date(this.formGroup.controls[key].value)
-              : this.formGroup.controls[key].value,
-          ],
+          values: [this.formGroup.controls[key].value],
         });
       }
     });
