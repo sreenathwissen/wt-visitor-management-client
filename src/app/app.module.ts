41f981ca32c3e9902d5e6c349f6104a9326199f6
SwapnaMohanty4554 <swapna.mohanty@wissen.com>
2023-06-02 13:23:37 +0530

    admin module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormModule } from './modules/form.module';
 import { GridModule } from './modules/grid.module';
-import { AdminComponent } from './components/admin/admin.component';
 import { AgGridModule } from 'ag-grid-angular';
-import { CheckoutComponent } from './components/admin/checkout/checkout.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { CheckoutPopupComponent } from './sharedModules/checkout-popup/checkout-popup.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { AdminModule } from './modules/admin.module';
+
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, AdminComponent, CheckoutComponent, CheckoutPopupComponent],
+  declarations: [AppComponent, NavbarComponent],
   providers: [],
   bootstrap: [AppComponent],
   imports: [
@@ -28,16 +23,9 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     FormModule,
     GridModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
+    AdminModule
   ],
   exports: [
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
   ],
 })
 export class AppModule {}
